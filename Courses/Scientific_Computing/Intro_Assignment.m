8376776df650454a2b8ce09f159e1363923f53aa
Brady Metherall <brady.metherall@maths.ox.ac.uk>
2019-10-17 08:36:11 +0100

    Finished MATLAB intro assignment

--- Courses/Scientific_Computing/Intro_Assignment.m
+++ Courses/Scientific_Computing/Intro_Assignment.m
@@ -1,20 +1,26 @@
+% Brady Metherall
+
 % Exercise 1.1
+disp('Exercise 1.1')
 0.8 * (7/13 + 5/7)
 (2-3i)/(1+(1-4i)/(3-2i))
 1/sqrt(1-(5*10^7/(8*10^8))^2)
 
 % Exercise 1.2
+disp('Exercise 1.2')
 A = magic(3)
 d = diag(A)
 B = [0 d'; d A]
 size(B)
 
 % Exercise 1.3
+disp('Exercise 1.3')
 [-10:10]
 [7:-1/3:2]
 [2:0.05:3]*i
 
 % Exercise 1.4
+disp('Exercise 1.4')
 d'*d
 d*d'
 A'*d
@@ -27,6 +33,7 @@ det(A)
 eig(A)
 
 % Exercise 1.5
+disp('Exercise 1.5')
 M = magic(5)
 m = M(:)
 m2 = flip(m)
@@ -37,6 +44,7 @@ prod(size(M2))
 numel(M2)
 
 % Exercise 1.6
+disp('Exercise 1.6')
 format long
 t = linspace(0,1,10);
 integrand = 2 / sqrt(pi) * exp(-t.^2);
@@ -50,12 +58,14 @@ for i = 1:5
 end
 results
 
-loglog(results(:,1), abs(results(:,2) - erf(1)))
+loglog(results(:,1), abs(results(:,2) - erf(1)),'-s')
+grid on
 
 f = @(t) 2 / sqrt(pi) * exp(-t.^2);
 quad(f, 0, 1)
 
 % Exercise 1.7
+disp('Exercise 1.7')
 x = linspace(0,1,10^5);
 trapz(x, exp(x.^3))
 
