99cc416d9df2304c6cb7f4def0b073adf93b558c
Brady Metherall <metherall@maths.ox.ac.uk>
2019-11-15 18:25:21 +0000

    Cleaned code

--- Courses/Computational_Techniques/SVD.py
+++ Courses/Computational_Techniques/SVD.py
@@ -1,26 +1,47 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def rand_SVD(A, r, l):
+def Full_Rank(m, n):
+	# Random mxn with full rank (P = 1)
+	return np.random.normal(size = (m, n))
+
+def Part_Rank(m, n, r):
+	# Random mxn with rank r (P = 1)
+	return np.matmul(np.random.normal(size = (m, r)), np.random.normal(size = (r, n)))
+
+# Randomized Truncated SVD
+def Rand_SVD(A, r, l, full = False):
 	m, n = A.shape
 	Omega = np.random.normal(size = (n, r + l))
 	Q, R = np.linalg.qr(np.matmul(A, Omega))
 	U, Sigma, V = np.linalg.svd(np.matmul(Q.T, A), full_matrices = False)
-	return np.matmul(Q, U)[:, :r], Sigma[:r], V[:r]
-
-r = 140
-A = np.random.normal(size = (250, 150))
-
-u, s, v = rand_SVD(A, r, 5)
-Arand = np.matmul((u * s), v)
+	if full:
+		return np.matmul(Q, U)[:, :r], Sigma[:r], V[:r] # Return individual matrices
+	else:
+		return np.matmul((np.matmul(Q, U)[:, :r] * Sigma[:r]), V[:r]) # Return the approximation
 
 
-ur, sr, vr = np.linalg.svd(A, full_matrices = False)
-Ar = np.matmul((ur[:, :r] * sr[:r]), vr[:r])
+# Dimensions
+m = 500
+n = 100
+#r = 50
+l = 1
 
-print sr[r]
-print np.linalg.norm(A - Ar, ord = 2)
-print np.linalg.norm(A - Arand, ord = 2)
+err = np.zeros(n)
 
-print np.linalg.norm(A - Arand, ord = 2) / np.linalg.norm(A - Ar, ord = 2)
+for r in range(n):
+	# Full Rank
+	A = Full_Rank(m, n)
 
+	# Rank r
+	#A = Part_Rank(m, n, r)
 
+	Arand = Rand_SVD(A, r, l)
+
+	ur, sr, vr = np.linalg.svd(A, full_matrices = False)
+	Ar = np.matmul((ur[:, :r] * sr[:r]), vr[:r])
+
+	err[r] = np.linalg.norm(A - Arand, ord = 2) / np.linalg.norm(A - Ar, ord = 2)
+
+plt.plot(range(n), err)
+plt.show()
