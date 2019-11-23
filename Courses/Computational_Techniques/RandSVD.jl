e0d73e3926eec703d30b4ad4ac72ac36432c8b6a
Brady Metherall <metherall@maths.ox.ac.uk>
2019-11-23 18:59:37 +0000

    Almost finished writing results

--- Courses/Computational_Techniques/RandSVD.jl
+++ Courses/Computational_Techniques/RandSVD.jl
@@ -37,9 +37,6 @@ end
 	Q = Array(qr(A * Omega).Q)
 	C = svd(transpose(Q) * A)
 	U, V = Array(C.U), Array(C.Vt) # Take leading parts
-	#println(size(Q * U))
-	#println(size(Diagonal(Array(C.S))))
-	#println(size(V))
 	return (Q * U)[:,1:r] * Diagonal(Array(C.S[1:r])) * V[1:r,:]
 end
 
@@ -79,10 +76,39 @@ function Solve(m::Int64, n::Int64, N::Int64 = 25, l::Int64 = 5, rmax::Int64 = n 
     close(f)
 end
 
+# Main function to compute errors for rankr
+function SolveR(m::Int64, n::Int64, N::Int64 = 25, l::Int64 = 5, rmax::Int64 = n - l; fname = "RankConverge.dat")
+    f = open(fname, "w");
+	for i in 1:rmax # Loop over ranks
+		res = SharedArray{Float64, 2}((N, 6)) # Results array (shared between all workers)
+		println(i)
+		# Perform the decomposition N times to get average errors
+		@sync @distributed for j in 1:N # Parallel for loop
+			A = PartRank(m, n, i)
+
+			Arand = RandSVD(A, i, l)
+			Atrunc = TruncSVD(A, i)
+
+			# 2 norm, F norm, trace norm
+			res[j,:] = [opnorm(A - Arand) opnorm(A - Atrunc) norm(A - Arand) norm(A - Atrunc) opnorm(A - Arand, 1) opnorm(A - Atrunc, 1)]
+		end
+		# Compute means and standard deviations for the three norms
+		avg2 = mean(res[:,1])
+		avgtrunc2 = mean(res[:,2])
+		avgF = mean(res[:,3])
+		avgtruncF = mean(res[:,4])
+		avgT = mean(res[:,5])
+		avgtruncT = mean(res[:,6])
+		writedlm(f, [i avg2 avgtrunc2 avgF avgtruncF avgT avgtruncT])
+    end
+    close(f)
+end
+
 # Compile
 RandSVD(randn(8, 5), 4, 2)
 TruncSVD(randn(8, 5), 4)
 Solve(2, 2, 2, 1)
+SolveR(2, 2, 2, 1)
 
 m = 500
 n = 250
@@ -90,10 +116,9 @@ l = 5
 
 N = 1000
 
-RandSVD(FullRank(m,n),50)
-RandSVD(FullRank(m,n),50)
-
 #Solve(m, n, N, l; fname = "FullRankBig.dat")
 #Solve(m, n, N, l; fname = "PartRankBig.dat")
 #Solve(m, n, N, l; fname = "GeoBig.dat")
-Solve(m, n, N, l; fname = "Test.dat")
+#Solve(m, n, N, l; fname = "AlgBig.dat")
+
+SolveR(m, n, N, l; fname = "RankConverge.dat")
