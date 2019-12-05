92c3e0d695154ac1b729dafc3db4c78d49680952
Brady Metherall <metherall@maths.ox.ac.uk>
2019-12-05 17:26:44 +0000

    Update code

--- Courses/Mathematical_Analytics/EM_Alg.jl
+++ Courses/Mathematical_Analytics/EM_Alg.jl
@@ -2,6 +2,7 @@
 #using LinearAlgebra
 using DelimitedFiles
 using Distributions
+using Random
 
 function get_data(lambda, pis, N = 10^4)
     pis /= sum(pis)
@@ -21,7 +22,7 @@ function get_pi(tau)
     return sum(tau, dims = 2) ./ size(tau, 2)
 end
 
-function iterate(lambda, pis, tol = 10^-6, Nmax = 10^3)
+function iterate(x, lambda, pis, tol = 10^-4, Nmax = 10^3; output = false)
     lambdaold = 10^10 .* ones(length(lambda))
     i = 0
     while sum(abs.(lambda - lambdaold)) > tol
@@ -29,31 +30,41 @@ function iterate(lambda, pis, tol = 10^-6, Nmax = 10^3)
         tau = get_tau(x, lambda, pis)
         lambda = get_lambda(x, tau)
         pis = get_pi(tau)
+
+        if output
+            writedlm("./Animate/Fake$i.dat", sum(transpose(length(x) .* pis .* broadcast(pdf, Poisson.(lambda), transpose(0:100))), dims = 2))
+        end
+
         i += 1
         if i > Nmax
             println("problem")
             break
         end
     end
+    println(i)
     return lambda, pis
 end
 
-#x = vec(readdlm("Sale_Data.dat", Int64))
+Random.seed!(5050)
 
-dat_lam = [10, 40, 70]
-dat_pi = [1, 1, 1]
+#x = vec(readdlm("SaleData.dat", Int64))
 
-x = get_data(dat_lam, dat_pi, 10^5)
+dat_lam = [12, 20, 40, 65]
+dat_pi = [1, 2, 3, 2]
 
-writedlm("Poisson.dat", sort(x))
+x = get_data(dat_lam, dat_pi, 10^4)
 
-lambda0 = (maximum(x) - minimum(x)) / (length(dat_lam)) .* collect(1:length(dat_lam)) .+ minimum(dat_lam)
+writedlm("FakeData.dat", sort(x))
 
-#lambda0 = [15, 45, 60]
+K = 4
+
+#lambda0 = (maximum(x) - minimum(x)) / (length(dat_lam) + 1) .* collect(1:K) .+ minimum(dat_lam)
+
+lambda0 = [10, 25, 50, 80]
 
 pis0 = rand(length(lambda0))
 pis0 /= sum(pis0)
 
-iterate(lambda0, pis0, 1) # Compile
+iterate(x, lambda0, pis0, 10^2) # Compile
 
-@time iterate(lambda0, pis0)
+@time println(iterate(x, lambda0, pis0; output = true))
