f734f2aae5cf2ceb6570ba64fa4c1524a036eadf
Brady Metherall <metherall@maths.ox.ac.uk>
2019-12-17 14:16:32 +0000

    Attempted file reading

--- Courses/C++/Main.cpp
+++ Courses/C++/Main.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 #include <array>
 #include "cmath"
+#include "fstream"
+#include "sstream"
+#include "string"
+#include "cassert"
 
 using namespace std;
 
 int main(int argc, char *argv[]){
 
-	// array<int, 10> a;
-	//
-	// for (int i = 0; i < 10; i++) {
-	// 	cout << a[i] << '\n';
-	// }
+	/*
+	array<int, 10> a;
 
-	// int N = 10000;
-	//
-	// double s = 0.0;
-	//
-	// for (int i = 1; i <= N; i++) {
-	// 	s += 1.0 / (i * i);
-	// }
-	//
-	// cout << sqrt(6 * s) << '\n';
+	for (int i = 0; i < 10; i++) {
+		cout << a[i] << '\n';
+	}
+	*/
 
+	/*
+	int N = 10000;
+
+	double s = 0.0;
+
+	for (int i = 1; i <= N; i++) {
+		s += 1.0 / (i * i);
+	}
+
+	cout << sqrt(6 * s) << '\n';
+	*/
+
+	/*
 	array<double, 4> a = {1.0, 1.0 / sqrt(2.0), 1.0 / 4, 1.0};
 	array<double, 4> anew;
 	int N = 3;
@@ -38,8 +47,64 @@ int main(int argc, char *argv[]){
 	}
 
 	cout << (a[0] + a[1]) * (a[0] + a[1]) / (4.0 * a[2]) << endl;
+	*/
 
-	cout << rand();
+	/*
+	const int N = pow(10, 4);
+	double dx = 1.0 / N;
+
+	array<double, N> x;
+	array<double, N> y;
+
+	x[0] = 0.0;
+	y[0] = 1.0;
+
+	for (int i = 1; i < N; i++) {
+		x[i] = i * dx;
+	}
+
+	for (int i = 1; i < N; i++) {
+		y[i] = y[i-1] / (1.0 + dx);
+	}
+
+	ofstream out("Euler.dat");
+	out.setf(ios::scientific|ios::showpos);
+
+	for (int i = 0; i < N; i++) {
+		out << x[i] << ' ' << y[i] << '\n';
+	}
+
+	out.close();
+	*/
+
+
+	const int N = pow(10, 4);
+
+	array<double, N> x;
+	array<double, N> y;
+
+	string line;
+	ifstream input("Euler.dat");
+	assert(input.is_open());
+
+	for (int i = 0; i < N-2; i++) {
+		istringstream s(line);
+		s >> x[i] >> y[i];
+		cout << y[i] << endl;
+	}
+
+	input.close();
+
+	double worst = 0.0;
+
+	for (int i = 0; i < N; i++) {
+		if (abs(y[i] - exp(-x[i])) > worst) {
+			worst = abs(y[i] - exp(-x[i]));
+		}
+	}
+
+	cout << worst << endl;
+
 
 	return 0;
 }
